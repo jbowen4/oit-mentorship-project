fb6b722913451f4f7c1ff4760b67b47c698d9a94
Ian Mitchell <ianmitch90@gmail.com>
2021-04-19 12:58:27 -0400

    chore: updated ignore

--- packages/ui-components/dist/utils/myuihelper/index.js
+++ packages/ui-components/dist/utils/myuihelper/index.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 exports.myUIhelper = void 0;
 function myUIhelper() {
-    console.log("myUIhelper called");
+    console.log("myUIhelper called im converting some sort of string into a number");
     return true;
 }
 exports.myUIhelper = myUIhelper;
